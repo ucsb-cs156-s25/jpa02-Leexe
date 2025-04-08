1a576561ccc403f1de2ede753ddc8c1d1ea731fc
Tuan Le <tuancle@ucsb.edu>
2025-04-08 14:00:43 -0700

    xy - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,6 +25,11 @@ public class DeveloperTest {
         assertEquals("Tuan L.", Developer.getName());
     }
 
+    @Test
+    public void getGithubId_returns_correct_githubID() {
+        assertEquals("Leexe", Developer.getGithubId());
+    }
+
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
